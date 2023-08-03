41926fea4ba7e846cc4bde3b62fb39d414835177
M7modElhalaq <mahmoudvee23@gmail.com>
2023-08-03 15:44:54 +0300

    bottom nav bar files

---
+++ lib/features/shopping_bag/data/models/model.dart
@@ -0,0 +1,1 @@
+// TODO: create any home feature model
---
+++ lib/features/shopping_bag/presentation/views/shopping_bag_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShoppingBagView extends StatelessWidget {
+  const ShoppingBagView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ShoppingBagView'),
+      ),
+    );
+  }
+}
