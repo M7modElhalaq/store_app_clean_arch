7c2be915fcbd5a062e1a5a4a952f8e01423ed1e0
M7modElhalaq <mahmoudvee23@gmail.com>
2023-08-15 15:05:51 +0300

    create product repo and usecases

--- lib/core/resources/manager_icon_sizes.dart
+++ lib/core/resources/manager_icon_sizes.dart
@@ -4,4 +4,5 @@ class ManagerIconSizes {
   static const double s30 = 30;
   static const double s36 = 36;
   static const double s40 = 40;
+  static const double s50 = 50;
 }
---
+++ lib/features/home/domain/repos/product_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_app/core/errors/failure.dart';
+import 'package:store_app/features/home/domain/entities/product.dart';
+
+abstract class ProductRepo {
+  Future<Either<Failure, List<Product>>> getOfferProducts();
+  Future<Either<Failure, List<Product>>> getTopSellingProducts();
+  Future<Either<Failure, List<Product>>> getNewProducts();
+}
