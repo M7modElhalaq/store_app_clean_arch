883be8f2f226d255a84295a53e23b5df509f36ee
M7modElhalaq <mahmoudvee23@gmail.com>
2023-09-05 18:53:43 +0300

    create ShoppingBagController

---
+++ lib/features/shopping_bag/presentation/controller/shopping_bag_controller.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:store_app/features/shopping_bag/domain/model/shopping_bag_model.dart';
+import 'package:store_app/features/shopping_bag/domain/use_cases/get_shopping_bag_data_use_case.dart';
+
+import '../../../../config/dependancy_injection.dart';
+import '../../../../core/resources/manager_height.dart';
+import '../../../../core/resources/manager_strings.dart';
+import '../../../../core/resources/manager_width.dart';
+import '../../../../core/widgets/helpers.dart';
+import '../../domain/model/shopping_bag_data_orders_model.dart';
+
+class ShoppingBagController extends GetxController with Helpers, StateMixin {
+  final GetShoppingBagDataUseCase _useCase = sl<GetShoppingBagDataUseCase>();
+  late ShoppingBagModel products;
+  late List<ShoppingBagDataOrdersModel> orders;
+  int isLoading = 0;
+  String errorMessage = ManagerStrings.somethingWentWrong;
+
+  @override
+  void onInit() async {
+    super.onInit();
+
+    isLoading = 0;
+    update();
+
+    await getShoppingBagData();
+
+    isLoading = 1;
+    update();
+  }
+
+  Future<void> getShoppingBagData() async {
+    isLoading = 0;
+    update();
+    BuildContext context = Get.context as BuildContext;
+    (await _useCase.execute()).fold(
+          (l) {
+        errorMessage = l.message.toString();
+        isLoading = 2;
+        showDialog(
+          context: context,
+          builder: (context) {
+            return SizedBox(
+              width: ManagerWidth.w120,
+              height: ManagerHeights.h100,
+              child: Center(
+                child: Text(l.message.toString()),
+              ),
+            );
+          },
+        );
+      },
+          (r) {
+        products = r;
+        orders = products.data.onProgress;
+      },
+    );
+    update();
+  }
+}
